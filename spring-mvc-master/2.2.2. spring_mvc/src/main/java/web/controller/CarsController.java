82a8b0cdf7c680d7555bd2156a608e13e0142178
Svyatoslav Panifidkin <svyatoslavchikviking@gmail.ru>
2020-08-10 14:00:07 +0700

    Progect_2_2_1 edited by spring_mvc-master -2

--- spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,17 +1,37 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
 import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CarsController {
 
+    @Autowired
+    private CarService carService;
+
+    public void setCarService(CarService carService) {
+        this.carService = carService;
+    }
+
+    public CarService getCarService() {
+        return carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(Model model) {
-        CarService carService = new CarService();
-        model.addAttribute("carsList", carService.getAllCar());
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Toyota", "Corolla", 123));
+        carList.add(new Car("Honda", "Jazz", 456));
+        carList.add(new Car("Nissan", "AD", 789));
+        carService.setCarList(carList);
+        model.addAttribute("carsList", carService.getCarList());
         return "cars";
     }
 }
--- spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -1,19 +1,19 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
-
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarService {
     private List<Car> carList = new ArrayList<>();
-    {
-        carList.add(new Car("Toyota", "Corolla", 123));
-        carList.add(new Car("Honda", "Jazz", 456));
-        carList.add(new Car("Nissan", "AD", 789));
+
+    public void setCarList(List<Car> carList) {
+        this.carList = carList;
     }
 
-    public List<Car> getAllCar() {
+    public List<Car> getCarList() {
         return carList;
     }
 }
